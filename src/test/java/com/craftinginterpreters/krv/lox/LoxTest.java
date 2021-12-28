d359ae16b8354788489e2cc34ed1b7200111ad2b
KRVPerera <rukshan.viduranga@gmail.com>
2021-12-28 12:04:12 +0530

    Add test

---
+++ src/test/java/com/craftinginterpreters/krv/lox/LoxTest.java
@@ -0,0 +1,18 @@
+package com.craftinginterpreters.krv.lox;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoxTest {
+
+    @Test
+    void test1() throws IOException {
+//        Lox.runFile("src/test/resources/com/craftinginterpreters/krv/lox/global.lox");
+    }
+
+}
